b7e3fec0a7f632e760f5f5c3ab0ccf40f9064854
Ady Shimony <ady.shimony@gmail.com>
2018-02-03 22:05:14 +0200

    Add bulk fetch and insert

--- indexer/src/indexer/indexerHistory.ts
+++ indexer/src/indexer/indexerHistory.ts
@@ -1,6 +1,7 @@
 import * as logger from 'winston'
 import * as blockchainUtils from '../utils/blockchainUtils'
 import * as dbUtils from '../utils/dbUtils'
+import * as utils from '../utils/utils'
 import { IndexerSettings } from '../indexer/indexerSettings'
 import { configuration } from '../config/config'
 
@@ -11,30 +12,32 @@ export class IndexerHistory{
     this.web3 = new Web3()
     this.web3.setProvider(configuration.provider)  
     this.indexSetttings = new IndexerSettings()
+    this.accountCount = 0
   }
 
   web3: any
   indexSetttings : IndexerSettings
+  accountCount : number
+
 
   async startIndexerProcess () {
     logger.info('startIndexerProcess')
   
-
-
-
+    var totalStartTime = process.hrtime();
     // first time
     try {
       this.indexSetttings = await dbUtils.getIndexerSettingsAsync(this.indexSetttings.id)
-      var startTime = process.hrtime();
+      let startTime = process.hrtime();
       let last = this.indexSetttings.lastBlockNumber
       await this.startIndex(this.indexSetttings.lastBlockNumber, this.indexSetttings.endBlockNumber) 
-      var elapsedSeconds = this.parseHrtimeToSeconds(process.hrtime(startTime));
+      let elapsedSeconds = utils.parseHrtimeToSeconds(process.hrtime(startTime));
       logger.info(`startIndexerProcess method, ${this.indexSetttings.endBlockNumber - last} blocks, duration in sec: ${elapsedSeconds}`)          
     } catch (error) {
       // first chunk, no settings in db yet
       logger.info(`startIndexerProcess, first chunk, block # 0 to block # ${configuration.BlockChunkSize}`)
-      this.indexSetttings.startBlockNumber = 45000 // no transactions before this block, we can index from here
-      this.indexSetttings.endBlockNumber = 46000  
+      this.indexSetttings.startBlockNumber = 46000 // no transactions before this block, we can index from here
+      this.indexSetttings.lastBlockNumber = 46000
+      this.indexSetttings.endBlockNumber = 46000 + configuration.BlockChunkSize
       await dbUtils.saveIndexerSettingsAsync(this.indexSetttings)
       await this.startIndex(this.indexSetttings.startBlockNumber, this.indexSetttings.endBlockNumber) 
     }
@@ -45,6 +48,8 @@ export class IndexerHistory{
     do {      
       highestBlock = await this.web3.eth.getBlock('pending')
       highestBlockNumber = highestBlock.number - configuration.MaxEphemeralForkBlocks
+      highestBlockNumber = 76000 // temp patch
+
       logger.info(`startIndexerProcess, highstBlock: ${highestBlockNumber}`)
 
       this.indexSetttings.startBlockNumber = this.indexSetttings.endBlockNumber 
@@ -53,13 +58,18 @@ export class IndexerHistory{
       if(this.indexSetttings.endBlockNumber > highestBlockNumber)
         this.indexSetttings.endBlockNumber = highestBlockNumber
 
-      var startTime = process.hrtime();
+      let startTime = process.hrtime();
       await this.startIndex(this.indexSetttings.startBlockNumber, this.indexSetttings.endBlockNumber) 
-      var elapsedSeconds = this.parseHrtimeToSeconds(process.hrtime(startTime));
+      let elapsedSeconds = utils.parseHrtimeToSeconds(process.hrtime(startTime));
       logger.info(`startIndexerProcess method, ${this.indexSetttings.endBlockNumber - this.indexSetttings.startBlockNumber} blocks, duration in sec: ${elapsedSeconds}`)                
       logger.info(`startIndexerProcess, indexSetttings: ${JSON.stringify(this.indexSetttings)}`)
 
     } while(this.indexSetttings.endBlockNumber < highestBlockNumber)
+    logger.info(`startIndexerProcess finished index history to ,highestBlock: ${highestBlockNumber}`)
+    var totalElapsedSeconds = utils.parseHrtimeToSeconds(process.hrtime(totalStartTime));
+    logger.info(`startIndexerProcess history finished, duration in sec: ${totalElapsedSeconds}`)      
+    
+    await this.Validate();      
   }
 
   async startIndex (startBlock: number, endBlock: number) : Promise<void> {
@@ -70,7 +80,7 @@ export class IndexerHistory{
     
       var startTime = process.hrtime();
       await this.indexBlockRange(start, end)
-      var elapsedSeconds = this.parseHrtimeToSeconds(process.hrtime(startTime));
+      var elapsedSeconds = utils.parseHrtimeToSeconds(process.hrtime(startTime));
       logger.info(`startIndex method, ${configuration.BlockStep} blocks, duration in sec: ${elapsedSeconds}`)
       
       startBlock += configuration.BlockStep
@@ -85,11 +95,29 @@ export class IndexerHistory{
   async indexBlockRange (startBlock: number, endBlock: number) : Promise<void> {
     let acccounts = await blockchainUtils.getAccountsAsync(startBlock, endBlock)
     logger.info(`total accounts ${acccounts.size} from block #${startBlock} to block #${endBlock}.`)
-    await dbUtils.saveAccountsAsync(acccounts)
+    
+    configuration.UseBulk ? 
+      await dbUtils.saveAccountsBulkAsync(acccounts)
+    : await dbUtils.saveAccountsAsync(acccounts)
+    
+    this.accountCount += acccounts.size
+    logger.info(`total accounts so far ${this.accountCount}.`)
   }
 
-  parseHrtimeToSeconds(hrtime) {
-    var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
-    return seconds;
-  }    
+  private async Validate() {
+    let accounts = await dbUtils.getAllDocsAsync();
+    let transactions = new Map<string, boolean>();
+    let transactionsCount = 0;
+    accounts.forEach(function (doc) {
+      // output each document's body
+      transactionsCount += doc.doc.transactions.length;
+      doc.doc.transactions.forEach(function (tran) {
+        if (!transactions.has(tran.hash))
+          transactions.set(tran.hash, true);
+        else
+          logger.info('error', 'duplicated transaction in DB!!!');
+      });
+    });
+    logger.info(`startIndexerProcess history finished, transactions count: ${transactionsCount}`);
+  }
 }
--- indexer/src/utils/dbUtils.ts
+++ indexer/src/utils/dbUtils.ts
@@ -1,4 +1,5 @@
 import * as logger from 'winston'
+import * as utils from '../utils/utils'
 import { Account } from '../../../common/models/account'
 import { dbHandler } from '../utils/couchdb'
 import { configuration } from '../config/config'
@@ -7,41 +8,119 @@ import { IndexerSettings } from '../indexer/indexerSettings'
 const historyDb = dbHandler.use(configuration.HistoryDBName)
 const settingsDb = dbHandler.use(configuration.SettingDBName)
 
-// TODO - move all to promises
-// accounts functions
-export async function saveAccountAsync (account: any) : Promise<void> {
-//logger.info(`saving account # ${account.address}`)
-return new Promise<void>((resolve, reject) => {
-  historyDb.get(account.address, function (error, existing) {
-    if (!error) {
-     // logger.info(`account # ${account.address} exist, updating revision`)
-      account._rev = existing._rev
-      account.transactions = account.transactions.concat(existing.transactions)
-    }
-    historyDb.insert(account, account.address, function (error, response) {
-      if (!error) {
-       // logger.info(`account # ${account.address} inserted`)
-        resolve()
+// bulk accounts functions
+export async function saveAccountsBulkAsync (accounts: Map<string, any>) : Promise<void> {
+  let totalStartTime = process.hrtime();
+  logger.info(`saving ${accounts.size} accounts`)
+  await updateAccountRevisionsBulkAsync(accounts)
+  return new Promise<void>(async (resolve, reject) => {
+    let accountsList = []
+    accounts.forEach(async (value: any, key: string) => {
+      value._id = key
+      accountsList.push(value)
+    })
+    historyDb.bulk({docs:accountsList}, async function(err, body) {
+      if(!err) {
+        let rejectedAccounts = [] 
+        for(let index = 0 ; index < body.length;  index++) {
+          if(body[index].hasOwnProperty('error')) {
+              rejectedAccounts.push(body[index].id)
+          }
+      }
+      logger.info('rejectedAccounts')
+      logger.info(JSON.stringify(rejectedAccounts))
+      let elapsedSeconds = utils.parseHrtimeToSeconds(process.hrtime(totalStartTime));
+      logger.info(`saveAccountsBulkAsync ${accounts.size} accounts, duration in sec: ${elapsedSeconds}`)                      
+      resolve()
       } else {
-        logger.log('error', `error creating account # ${account.address}, ${error}`)
-        reject(new Error(`error creating account # ${account.address}, ${error}`))
+        reject(new Error(`error saveAccountsBulkAsync : ${err}`))
       }
     })
   })
-})
 }
 
+
+export async function updateAccountRevisionsBulkAsync(accounts: any): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    let accountsList = []
+    accounts.forEach(async (value: any, key: string) => {
+      accountsList.push(key)
+    })
+    historyDb.fetch({ keys: accountsList }, async function (err, revResultList) {
+      //logger.info('revResultList')
+      //logger.info(JSON.stringify(revResultList))
+      if (!err) {
+        let restAccounts = []
+        for (let index = 0; index < revResultList.rows.length; index++) {
+          if (revResultList.rows[index].hasOwnProperty('error')) {
+            continue
+          } else {
+            let account = accounts.get(revResultList.rows[index].key)
+            account.transactions = account.transactions.concat(revResultList.rows[index].doc.transactions)
+            account._rev = revResultList.rows[index].doc._rev
+          }
+        }
+        resolve()
+      } else {
+        reject(new Error(`error updateAccountRevisionsBulkAsync : ${err}`))
+      }
+    })
+  })
+}
+
+// non bulk accounts functions
 export async function saveAccountsAsync (accounts: Map<string, Account>) : Promise<void> {
   logger.info(`saving ${accounts.size} accounts`)
+  let totalStartTime = process.hrtime();
+  logger.info(`saving ${accounts.size} accounts`)  
   let savePromises = []
-  accounts.forEach(async (value: Account, key: string) => {
-    savePromises.push(saveAccountAsync(value))
+  accounts.forEach((value: Account, key: string) => {
+    savePromises.push(saveSingleAccountAsync(value))
   })
   await Promise.all(savePromises)
   logger.info(`${accounts.size} accounts saved`)
+  let elapsedSeconds = utils.parseHrtimeToSeconds(process.hrtime(totalStartTime));
+  logger.info(`saveAccountsAsync ${accounts} accounts, duration in sec: ${elapsedSeconds}`)     
 }
 
+export async function saveSingleAccountAsync (account: any) : Promise<void> {
+  //logger.info(`saving account # ${account.address}`)
+  return new Promise<void>((resolve, reject) => {
+    historyDb.get(account.address, function (error, existing) {
+      if (!error) {
+       // logger.info(`account # ${account.address} exist, updating revision`)
+        account._rev = existing._rev
+        account.transactions = account.transactions.concat(existing.transactions)
+      }
+      historyDb.insert(account, account.address, function (error, response) {
+        if (!error) {
+         // logger.info(`account # ${account.address} inserted`)
+          resolve()
+        } else {
+          logger.log('error', `error creating account # ${account.address}, ${error}`)
+          reject(new Error(`error creating account # ${account.address}, ${error}`))
+        }
+      })
+    })
+  })
+ }
+  
 // indexer settings functions
+export async function getIndexerSettingsAsync (indexerID) : Promise<any> {
+  logger.info(`fetching settings for # ${indexerID}`)
+  return new Promise((resolve, reject) => {
+    settingsDb.get(indexerID, async (error, existing) => {
+      if (!error) {
+        logger.info(`settings for indexer ${indexerID} found, ${JSON.stringify(existing)}`)
+        resolve(existing)
+      } else {
+        logger.log('error', `error fetching settings for indexer ${indexerID}`)
+        reject(new Error(`error fetching settings for indexer ${indexerID}`))
+      }
+    })
+  })
+}
+
 export async function saveIndexerSettingsAsync (settings: any) : Promise<void> {
   logger.info(`saving indexer settings  ${JSON.stringify(settings)}`)
   return new Promise<void>((resolve, reject) => {
@@ -63,17 +142,19 @@ export async function saveIndexerSettingsAsync (settings: any) : Promise<void> {
   })
 }
 
-export async function getIndexerSettingsAsync (indexerID) : Promise<any> {
-  logger.info(`fetching settings for # ${indexerID}`)
+export async function getAllDocsAsync ( ) : Promise<any> {
+  logger.info(`getAllDocsAsync`)
   return new Promise((resolve, reject) => {
-    settingsDb.get(indexerID, async (error, existing) => {
+    historyDb.list({include_docs: true}, async (error, body) => {
       if (!error) {
-        logger.info(`settings for indexer ${indexerID} found, ${JSON.stringify(existing)}`)
-        resolve(existing)
+        resolve(body.rows)
       } else {
-        logger.log('error', `error fetching settings for indexer ${indexerID}`)
-        reject(new Error(`error fetching settings for indexer ${indexerID}`))
+        logger.log('error', 'getAllDocsAsync')
+        reject(new Error(`getAllDocsAsync`))
       }
     })
   })
 }
+
+
+
