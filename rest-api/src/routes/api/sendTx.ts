aa2c813af41c33a99cb2719e56c0a13631690cf1
adyshimony <ady.shimony@gmail.com>
2018-06-28 16:50:59 +0300

    send transaction api skeleton

---
+++ rest-api/src/routes/api/sendTx.ts
@@ -0,0 +1,44 @@
+import * as express from 'express'
+import * as logger from 'winston'
+import * as blockUtils from '../../utils/blockUtils'
+import { configuration } from '../../config/config'
+import { Transaction } from '../../../../common/models/transaction'
+
+const router = express.Router()
+
+const Web3 = require('web3')
+const web3 = new Web3()
+
+web3.setProvider(configuration.provider)
+
+router.get('/sendTx/:signedTransaction', async (req, res, next) => {
+
+  // TODO: need to finish this
+  let signedTransaction = req.params.signedTransaction
+  var evt = await web3.eth.sendSignedTransaction(signedTransaction)
+
+  let result
+  let status = 0
+  let message = 'FAIL'
+
+  // if (transaction) {
+  //   try {
+  //     result = raw ? transaction : fullTransaction
+  //     message = 'OK'
+  //     status = 1
+  //   } catch (error) {
+  //     message = 'FAIL'
+  //     result = null
+  //     logger.info(error)
+  //   }
+  // }
+
+  return res.json(
+    {
+      'status': status,
+      'message': message,
+      'result': result
+    })
+})
+
+module.exports = router
