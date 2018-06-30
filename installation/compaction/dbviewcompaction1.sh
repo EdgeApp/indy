74c0a084a0eb2daf19dd1e6d04ba7fdf6cdb2ed2
adyshimony <ady.shimony@gmail.com>
2018-06-30 23:23:12 +0300

    Add destination view compaction

--- installation/compaction/dbviewcompaction1.sh
+++ installation/compaction/dbviewcompaction1.sh
@@ -8,38 +8,56 @@ PASSWORD="$2"
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000000000-000099999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000000000-000099999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000000000-000099999/_compact/destinationDocBlocks
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000100000-0000199999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000100000-0000199999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000100000-0000199999/_compact/destinationDocBlocks
+
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000200000-0000299999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000200000-0000299999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000200000-0000299999/_compact/destinationDocBlocks
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000300000-0000399999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000300000-0000399999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000300000-0000399999/_compact/destinationDocBlocks
+
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000400000-0000499999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000400000-0000499999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000400000-0000499999/_compact/destinationDocBlocks
+
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000500000-0000599999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000500000-0000599999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000500000-0000599999/_compact/destinationDocBlocks
+
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000600000-0000699999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000600000-0000699999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000600000-0000699999/_compact/destinationDocBlocks
+
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000700000-0000799999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000700000-0000799999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000700000-0000799999/_compact/destinationDocBlocks
+
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000800000-0000899999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000800000-0000899999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000800000-0000899999/_compact/destinationDocBlocks
+
 
 
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000900000-0000999999/_compact/fromDocBlocks
 curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000900000-0000999999/_compact/toDocBlocks
+curl -H "Content-Type: application/json" -X POST http://$USER:$PASSWORD@localhost:5984/supernodedb-0000900000-0000999999/_compact/destinationDocBlocks
+
